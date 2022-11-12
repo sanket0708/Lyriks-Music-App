39e3c7e932bc8b80bc0e7999facf678adc56a403
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-11-12 15:18:24 +0530

    Lyricks Music App

---
+++ src/pages/TopArtists.jsx
@@ -0,0 +1,26 @@
+import { ArtistCard, Loader, Error } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+const TopArtists = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching && loading) return <Loader title="Loading Top Charts..." />;
+
+  if (error && country !== "") return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Discover Top Artists
+      </h2>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((track) => (
+          <ArtistCard key={track.key} track={track} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopArtists;
